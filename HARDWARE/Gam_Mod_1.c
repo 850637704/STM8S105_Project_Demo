a2c4255cf40ebb4d74e885eebb77ef2f411455ae
XiaoKai <850637704@qq.com>
2017-12-22 18:55:28 +0800

    修改按泡泡模式(同时响应多个按键)，调试中...

--- HARDWARE/Gam_Mod_1.c
+++ HARDWARE/Gam_Mod_1.c
@@ -50,94 +50,178 @@ void Gam_Mod_1(uint8_t Key_Val)
 {
 	if(Gam_Mod_tmp0!=0x00)														// 表示还有按键没有按下
 	{
-		switch(Key_Val)
+		if(((Key_Val>>1)&0x01) == 0) // Do Key_1
 		{
-			case 0xfc: // Do Key_1
-			{
-				if((Gam_Mod_tmp0>>1)&0x01)
-				{	
-				  	Mod1_Sco_Val ++;
-					if(Mod1_Sco_Val==1000)
-					  	Mod1_Sco_Val = 0;
-				  	Talk(Gam_Sud_Val,Key_Val);
-					Gam_Mod_tmp0 &= 0xfc;
-					GPIO_WriteHigh(GPIOB,(GPIO_Pin_TypeDef)~Key_Val);
-				}
-			}break;
-			case 0xfa: // Re Key_2
-			{
-				if((Gam_Mod_tmp0>>2)&0x01)
-				{	
-	  				Mod1_Sco_Val ++;
-					if(Mod1_Sco_Val==1000)
-					  	Mod1_Sco_Val = 0;
-				  	Talk(Gam_Sud_Val,Key_Val);
-					Gam_Mod_tmp0 &= 0xfa;
-					GPIO_WriteHigh(GPIOB,(GPIO_Pin_TypeDef)~Key_Val);
-				}
-			}break;
-			case 0xf6: // Mi Key_3
-			{
-				if((Gam_Mod_tmp0>>3)&0x01)
-				{	
-		  			Mod1_Sco_Val ++;
-					if(Mod1_Sco_Val==1000)
-					  	Mod1_Sco_Val = 0;
-				  	Talk(Gam_Sud_Val,Key_Val);
-					Gam_Mod_tmp0 &= 0xf6;
-					GPIO_WriteHigh(GPIOB,(GPIO_Pin_TypeDef)~Key_Val);
-				}
-			}break;
-			case 0xee: // Fa Key_4 
-			{
-				if((Gam_Mod_tmp0>>4)&0x01)
-				{	
-		  			Mod1_Sco_Val ++;
-					if(Mod1_Sco_Val==1000)
-					  	Mod1_Sco_Val = 0;
-				  	Talk(Gam_Sud_Val,Key_Val);
-					Gam_Mod_tmp0 &= 0xee;
-					GPIO_WriteHigh(GPIOB,(GPIO_Pin_TypeDef)~Key_Val);
-				}
-			}break;
-			case 0xde: // So Key_5
-			{
-				if((Gam_Mod_tmp0>>5)&0x01)
-				{	
-			  		Mod1_Sco_Val ++;
-					if(Mod1_Sco_Val==1000)
-					  	Mod1_Sco_Val = 0;
-				  	Talk(Gam_Sud_Val,Key_Val);
-					Gam_Mod_tmp0 &= 0xde;
-					GPIO_WriteHigh(GPIOB,(GPIO_Pin_TypeDef)~Key_Val);
-				}
-			}break;
-			case 0xbe: // La Key_6
-			{
-				if((Gam_Mod_tmp0>>6)&0x01)
-				{	
-			  		Mod1_Sco_Val ++;
-					if(Mod1_Sco_Val==1000)
-					  	Mod1_Sco_Val = 0;
-				  	Talk(Gam_Sud_Val,Key_Val);
-					Gam_Mod_tmp0 &= 0xbe;
-					GPIO_WriteHigh(GPIOB,(GPIO_Pin_TypeDef)~Key_Val);
-				}
-			}break;
-			case 0x7e: // Xi Key_7
-			{
-				if((Gam_Mod_tmp0>>7)&0x01)
-				{	
-				  	Mod1_Sco_Val ++;
-					if(Mod1_Sco_Val==1000)
-					  	Mod1_Sco_Val = 0;
-				  	Talk(Gam_Sud_Val,Key_Val);
-					Gam_Mod_tmp0 &= 0x7e;
-					GPIO_WriteHigh(GPIOB,(GPIO_Pin_TypeDef)~Key_Val);
-				}
-			}break;
-			default:{};break;
+			if((Gam_Mod_tmp0>>1)&0x01)
+			{	
+				Mod1_Sco_Val ++;
+				if(Mod1_Sco_Val==1000)
+					Mod1_Sco_Val = 0;
+				Talk(Gam_Sud_Val,Key_Val);
+				Gam_Mod_tmp0 &= 0xfc;
+				GPIO_WriteHigh(GPIOB,GPIO_PIN_1);
+			}
 		}
+		if(((Key_Val>>2)&0x01) == 0) // Re Key_2
+		{
+			if((Gam_Mod_tmp0>>2)&0x01)
+			{	
+				Mod1_Sco_Val ++;
+				if(Mod1_Sco_Val==1000)
+					Mod1_Sco_Val = 0;
+				Talk(Gam_Sud_Val,Key_Val);
+				Gam_Mod_tmp0 &= 0xfa;
+				GPIO_WriteHigh(GPIOB,GPIO_PIN_2);
+			}
+		}
+		if(((Key_Val>>3)&0x01) == 0) // Mi Key_3
+		{
+			if((Gam_Mod_tmp0>>3)&0x01)
+			{	
+				Mod1_Sco_Val ++;
+				if(Mod1_Sco_Val==1000)
+					Mod1_Sco_Val = 0;
+				Talk(Gam_Sud_Val,Key_Val);
+				Gam_Mod_tmp0 &= 0xf6;
+				GPIO_WriteHigh(GPIOB,GPIO_PIN_3);
+			}
+		}
+		if(((Key_Val>>4)&0x01) == 0) // Fa Key_4 
+		{
+			if((Gam_Mod_tmp0>>4)&0x01)
+			{	
+				Mod1_Sco_Val ++;
+				if(Mod1_Sco_Val==1000)
+					Mod1_Sco_Val = 0;
+				Talk(Gam_Sud_Val,Key_Val);
+				Gam_Mod_tmp0 &= 0xee;
+				GPIO_WriteHigh(GPIOB,GPIO_PIN_4);
+			}
+		}
+		if(((Key_Val>>5)&0x01) == 0) // So Key_5
+		{
+			if((Gam_Mod_tmp0>>5)&0x01)
+			{	
+				Mod1_Sco_Val ++;
+				if(Mod1_Sco_Val==1000)
+					Mod1_Sco_Val = 0;
+				Talk(Gam_Sud_Val,Key_Val);
+				Gam_Mod_tmp0 &= 0xde;
+				GPIO_WriteHigh(GPIOB,GPIO_PIN_5);
+			}
+		}
+		if(((Key_Val>>6)&0x01) == 0) // La Key_6
+		{
+			if((Gam_Mod_tmp0>>6)&0x01)
+			{	
+				Mod1_Sco_Val ++;
+				if(Mod1_Sco_Val==1000)
+					Mod1_Sco_Val = 0;
+				Talk(Gam_Sud_Val,Key_Val);
+				Gam_Mod_tmp0 &= 0xbe;
+				GPIO_WriteHigh(GPIOB,GPIO_PIN_6);
+			}
+		}
+		if(((Key_Val>>7)&0x01) == 0) // Xi Key_7
+		{
+			if((Gam_Mod_tmp0>>7)&0x01)
+			{	
+				Mod1_Sco_Val ++;
+				if(Mod1_Sco_Val==1000)
+					Mod1_Sco_Val = 0;
+				Talk(Gam_Sud_Val,Key_Val);
+				Gam_Mod_tmp0 &= 0x7e;
+				GPIO_WriteHigh(GPIOB,GPIO_PIN_7);
+			}
+		}
+//		switch(Key_Val)
+//		{
+//			case 0xfc: // Do Key_1
+//			{
+//				if((Gam_Mod_tmp0>>1)&0x01)
+//				{	
+//				  	Mod1_Sco_Val ++;
+//					if(Mod1_Sco_Val==1000)
+//					  	Mod1_Sco_Val = 0;
+//				  	Talk(Gam_Sud_Val,Key_Val);
+//					Gam_Mod_tmp0 &= 0xfc;
+//					GPIO_WriteHigh(GPIOB,(GPIO_Pin_TypeDef)~Key_Val);
+//				}
+//			}break;
+//			case 0xfa: // Re Key_2
+//			{
+//				if((Gam_Mod_tmp0>>2)&0x01)
+//				{	
+//	  				Mod1_Sco_Val ++;
+//					if(Mod1_Sco_Val==1000)
+//					  	Mod1_Sco_Val = 0;
+//				  	Talk(Gam_Sud_Val,Key_Val);
+//					Gam_Mod_tmp0 &= 0xfa;
+//					GPIO_WriteHigh(GPIOB,(GPIO_Pin_TypeDef)~Key_Val);
+//				}
+//			}break;
+//			case 0xf6: // Mi Key_3
+//			{
+//				if((Gam_Mod_tmp0>>3)&0x01)
+//				{	
+//		  			Mod1_Sco_Val ++;
+//					if(Mod1_Sco_Val==1000)
+//					  	Mod1_Sco_Val = 0;
+//				  	Talk(Gam_Sud_Val,Key_Val);
+//					Gam_Mod_tmp0 &= 0xf6;
+//					GPIO_WriteHigh(GPIOB,(GPIO_Pin_TypeDef)~Key_Val);
+//				}
+//			}break;
+//			case 0xee: // Fa Key_4 
+//			{
+//				if((Gam_Mod_tmp0>>4)&0x01)
+//				{	
+//		  			Mod1_Sco_Val ++;
+//					if(Mod1_Sco_Val==1000)
+//					  	Mod1_Sco_Val = 0;
+//				  	Talk(Gam_Sud_Val,Key_Val);
+//					Gam_Mod_tmp0 &= 0xee;
+//					GPIO_WriteHigh(GPIOB,(GPIO_Pin_TypeDef)~Key_Val);
+//				}
+//			}break;
+//			case 0xde: // So Key_5
+//			{
+//				if((Gam_Mod_tmp0>>5)&0x01)
+//				{	
+//			  		Mod1_Sco_Val ++;
+//					if(Mod1_Sco_Val==1000)
+//					  	Mod1_Sco_Val = 0;
+//				  	Talk(Gam_Sud_Val,Key_Val);
+//					Gam_Mod_tmp0 &= 0xde;
+//					GPIO_WriteHigh(GPIOB,(GPIO_Pin_TypeDef)~Key_Val);
+//				}
+//			}break;
+//			case 0xbe: // La Key_6
+//			{
+//				if((Gam_Mod_tmp0>>6)&0x01)
+//				{	
+//			  		Mod1_Sco_Val ++;
+//					if(Mod1_Sco_Val==1000)
+//					  	Mod1_Sco_Val = 0;
+//				  	Talk(Gam_Sud_Val,Key_Val);
+//					Gam_Mod_tmp0 &= 0xbe;
+//					GPIO_WriteHigh(GPIOB,(GPIO_Pin_TypeDef)~Key_Val);
+//				}
+//			}break;
+//			case 0x7e: // Xi Key_7
+//			{
+//				if((Gam_Mod_tmp0>>7)&0x01)
+//				{	
+//				  	Mod1_Sco_Val ++;
+//					if(Mod1_Sco_Val==1000)
+//					  	Mod1_Sco_Val = 0;
+//				  	Talk(Gam_Sud_Val,Key_Val);
+//					Gam_Mod_tmp0 &= 0x7e;
+//					GPIO_WriteHigh(GPIOB,(GPIO_Pin_TypeDef)~Key_Val);
+//				}
+//			}break;
+//			default:{};break;
+//		}
 	}
 	else
 	{
